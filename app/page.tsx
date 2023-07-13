230b174f6e53d1514752f5a0e32a533c3646e76d
emanuel <hitallosoares1@gmail.com>
2023-07-13 13:51:07 -0300

    subindo aplicação

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,167 @@
-import Image from 'next/image'
-
+import "../style.css/style.css";
+import Image from "../public/images/Grupo 2098.png";
+import faceIcon from '../public/icons/Icon awesome-facebook-f.png'
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <div className="section1">
+        <div className="menu">
+          <h1 className="title">agência</h1>
+          <ul>
+            <li>Home</li>
+            <li>o que fazemos</li>
+            <li>Cases</li>
+          </ul>
+          <button className="contact">Fale conosco</button>
+        </div>
+        <div className="main">
+          <div className="conteudo">
+            <h1 className="title">Melhor agência de marketing do bairro</h1>
+            <p>
+              Somos uma agência de performance digital, aceleramos vendas e
+              aquisição de leads para grandes marcas.
+            </p>
+            <button className="button-aumentar-vendas">Aumentar vendas</button>
+          </div>
+          <div className="conteudo">
+            <img src="/images/grupo.png" />
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="section2">
+        <div className="conteudo-titulos">
+          <h1>
+            Neste bairro, não há agência
+            <br /> melhor. Garantimos.
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+      <div className="conteudo-blocos">
+        <div className="bloco">
+          <img src="/images/icon.6cd5c086.png" />
+          <h1>Digital Strategy</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna.
           </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+        </div>
+        <div className="bloco">
+          <img src="/images/icon.6cd5c086.png" />
+          <h1>Social Midia</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
+        </div>
+        <div className="bloco">
+          <img src="/images/icon.6cd5c086.png" />
+          <h1>Estratégia digital</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna.
           </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+        </div>
+        <div className="bloco">
+          <img src="/images/icon.6cd5c086.png" />
+          <h1>Social Midia</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna.
           </p>
-        </a>
+        </div>
       </div>
-    </main>
-  )
+      <div className="section3">
+        <div className="container-texto">
+          <h2>ENTRE EM CONTATO</h2>
+          <h1>Aumente seu resultado de vendas e performance</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna
+          </p>
+        </div>
+        <div className="container-formulario">
+          <form className="form">
+             <h1 id="h1-form">Fale com um especialista</h1>
+            <input type="text" name="name" placeholder="Nome Completo"></input>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email profissional"
+            ></input>
+            <input
+              type="text"
+              name="phone"
+              placeholder="Celular/Whatsapp"
+            ></input>
+            <input type="text" name="site" placeholder="Site"></input>
+            <select name="select">
+              <option value="valor1">Orçamento para Midia</option>
+              <option value="valor2" selected>Instagram</option>
+              <option value="valor3">Facebook</option>
+            </select>
+            <button
+            className="button"
+            type="submit"
+            placeholder="enviar"
+            >Enviar
+            </button>
+          </form>
+          <p>Ao enviar esse formulário, você reconhece que leu e concorda com a nossa <strong> Política de Privacidade.</strong></p>
+        </div>
+      </div>
+      <footer>
+        <div className="footer-content">
+          <div className="footer-contacts">
+            <h1>agência</h1>
+            <h2>0800 800 800</h2>
+            <h3>comercial@agencia.com.br</h3>
+            
+          </div>
+          <ul className="footer-list">
+            <li> <h3>menu</h3></li>
+            <li>
+              <a className="footer-link">Quem somos</a>
+            </li>
+            <li>
+              <a  className="footer-link">Cases</a>
+            </li>
+          </ul>
+          <ul className="footer-list">
+            <li> <h3>Conteúdo</h3></li>
+            <li>
+              <a className="footer-link">E-books</a>
+            </li>
+            <li>
+              <a  className="footer-link">Fórmulas prontas</a>
+            </li>
+          </ul>
+          <ul className="footer-list">
+            <li><h3>Social</h3></li>
+            <div className="social">
+            <li className="footer-icon">
+              <img className="icon"src='/icons/Icon awesome-facebook-f.png'/>
+            </li>
+            <li className="footer-icon">
+            <img className="icon" src='/icons/Icon awesome-instagram.png'/>
+            </li>
+            <li className="footer-icon">
+            <img className="icon" src='/icons/Icon awesome-linkedin.png'/>
+            </li>
+            <li className="footer-icon">
+            <img className="icon" src='/icons/Icon awesome-youtube.png'/>
+            </li>
+            </div>
+          </ul>
+        </div>
+        
+        <div className="footer-copyhigth">
+        ©2022 AGENCIA - Todos os direitos reservados.
+        </div>
+      </footer>
+    </div>
+  );
 }
